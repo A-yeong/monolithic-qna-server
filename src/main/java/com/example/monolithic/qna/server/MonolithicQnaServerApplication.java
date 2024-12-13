39f8cdb1047fd05427a8e04c1d4a1fa0bc6bcbdb
glass <silver20190204@gmail.com>
2024-12-13 20:34:42 +0900

    Start

---
+++ src/main/java/com/example/monolithic/qna/server/MonolithicQnaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.monolithic.qna.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithicQnaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithicQnaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/monolithic/qna/server/MonolithicQnaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monolithic.qna.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicQnaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
