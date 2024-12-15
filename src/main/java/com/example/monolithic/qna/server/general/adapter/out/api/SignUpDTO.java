70007a9dd7d64a8b692b8c7007360decbb6adf34
glass <silver20190204@gmail.com>
2024-12-15 17:37:49 +0900

    Add UserAuthAPI

---
+++ src/main/java/com/example/monolithic/qna/server/general/adapter/out/api/SignUpDTO.java
@@ -0,0 +1,12 @@
+package com.example.monolithic.qna.server.general.adapter.out.api;
+
+import com.example.monolithic.qna.server.user.adapter.in.other.request.UserSignUpRequest;
+
+/**
+ * <b> 회원가입 DTO </b>
+ */
+public record SignUpDTO(String id, String pw, String role) {
+    public static UserSignUpRequest to(String id, String pw, String role) {
+        return new UserSignUpRequest(id, pw, role);
+    }
+}
---
+++ src/main/java/com/example/monolithic/qna/server/general/adapter/out/api/UserAuthAPI.java
@@ -0,0 +1,28 @@
+package com.example.monolithic.qna.server.general.adapter.out.api;
+
+import com.example.monolithic.qna.server.user.adapter.in.other.UserAdapter;
+import com.example.monolithic.qna.server.user.adapter.in.other.response.MessageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * <b> 회원관리 API 요청 </b>
+ */
+@Component
+@RequiredArgsConstructor
+public class UserAuthAPI {
+    private final UserAdapter userInputAdapter;
+
+    /**
+     * <b> 회원가입을 다른 도메인에 요청 </b>
+     *
+     * @param id 아이디
+     * @param pw 비밀번호
+     * @param role 역할
+     * @return 응답
+     */
+    public String requestUserSignUp(String id, String pw, String role) {
+        MessageResponse messageResponse = userInputAdapter.signUp(SignUpDTO.to(id, pw, role));
+        return messageResponse.getResult();
+    }
+}
