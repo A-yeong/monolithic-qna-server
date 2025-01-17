bd10bccdbb3036f3b1e48c3f7c416acfae0e9a03
glass <silver20190204@gmail.com>
2025-01-17 23:55:09 +0900

    Fix

--- performance_tests/login_100.js
+++ performance_tests/login_100.js
@@ -2,14 +2,14 @@ import { login } from './userLogin.js';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 100, // 1초 동안 100명의 VU가 각각 1번의 요청을 보냄
-  duration: '1s', // 테스트 지속 시간
+  vus: 10,
+  duration: '1s',
 };
 
 export default function() {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 5; i++) {
     login('general', 'pw');
   }
 
-  sleep(0.01); // 잠시 대기, 여러 요청이 빠르게 발생하도록
+  sleep(0.5);
 }
