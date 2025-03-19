7a00cf007c3a8c63cfc552592a75f900972fbbbb
A-Yeong <dkdud064@kumoh.ac.kr>
2025-03-19 19:33:52 +0900

    fix: 포트 수정

--- performance_tests/userLogin.js
+++ performance_tests/userLogin.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 
 // 로그인 함수 (일반 유저 및 전문가 공통)
 export function login(userId, pw) {
-  const url = 'http://localhost:8081/api/v1/user/sign-in';
+  const url = 'http://localhost:8080/api/v1/user/sign-in';
   const payload = JSON.stringify({ userId, pw });
   const params = { headers: { 'Content-Type': 'application/json' } };
   http.post(url, payload, params);
